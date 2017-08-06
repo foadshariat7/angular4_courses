4da2411e91896cf332eaf34661e18152eb3ed0e6
Foad R ShariatPanahi <jesuschrist.love@hotmail.com>
2017-08-05 17:19:55 -0700

    DataBinding

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,6 +5,34 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  age: number = 36;
+  name: string = "Foad";
+  btnClick = "Button is not clicked!";
+  lastName = "";
+
+  allowClick = false;
+
+  constructor(){
+    setTimeout(() => {
+      this.allowClick = true;
+    }, 5000);
+  }  
+
+  ngOnInit(){
+
+  }
+
+  onButtonClicked(){
+    this.btnClick = "Button now is clicked!";
+  }
+
+  getName(){
+    return this.name;
+  }
+
+  onInputChange(event: Event){
+    this.lastName = (<HTMLInputElement>event.target).value;
+  }
 
 }
